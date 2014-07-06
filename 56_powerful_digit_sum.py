f54effeb6f23f86e452b56ff10f27811c2169886
Vadim Borisov <vadim.borisov@gmail.com>
2014-07-06 01:30:32 -0700

    Problem 56: Powerful digit sum

---
+++ 56_powerful_digit_sum.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+# A googol (10^100) is a massive number: one followed by one-hundred zeros;
+# 100^100 is almost unimaginably large: one followed by two-hundred
+# zeros. Despite their size, the sum of the digits in each number is only 1.
+
+# Considering natural numbers of the form, a^b, where a, b < 100, what is the
+# maximum digital sum?
+
+def GetPowDigitalSum(a, b):
+    return sum([int(i) for i in str(a**b)])
+
+print max([GetPowDigitalSum(a, b) for a in range(100) for b in range(100)])
